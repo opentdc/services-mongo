1a53cb3425c6cb10f5c5e3aa88d07236dfd41c35
Bruno Kaiser <bruno@bkaiser.ch>
2015-07-26 10:28:55 +0200

    mongodb access works

--- src/java/org/opentdc/mongo/AbstractMongodbServiceProvider.java
+++ src/java/org/opentdc/mongo/AbstractMongodbServiceProvider.java
@@ -23,159 +23,254 @@
  */
 package org.opentdc.mongo;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletContext;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.opentdc.service.exception.InternalServerErrorException;
+import org.opentdc.service.exception.NotFoundException;
+import org.opentdc.service.exception.ValidationException;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+
+import static com.mongodb.client.model.Filters.*;
+
+
+/**
+ * Abstract implementation of mongodb access.
+ * Use it as follows:  connect(); getCollection(name); your operations; disconnect().
+ * The data must be converted into a org.bson.Document first.
+ * @author Bruno Kaiser
+ *
+ * @param <T>
+ */
 public class AbstractMongodbServiceProvider<T> {
 	private static final Logger logger = Logger.getLogger(AbstractMongodbServiceProvider.class.getName());
+	private static String mongodbHost = null;
+	private static short mongodbPort = 0;
+	private static MongoClient mongoClient = null;  // represents a pool of connections for a database cluster
+	protected MongoCollection<Document> collection = null; // corresponds to a table
+	protected String collectionName = null;
+	private static final String DB_NAME = "opentdc_v1";
 	
+	/**
+	 * Constructor
+	 * @param context  the servlet context with configuration attributes
+	 */
 	public AbstractMongodbServiceProvider(
-		ServletContext context,
-		String prefix
-	) throws IOException {
-		String _value = context.getInitParameter("isPersistent");  // true (= file) or false (= transient), default is true
-		if (_value != null) {
-			isPersistent = new Boolean(_value).booleanValue();
+		ServletContext context) 
+	{
+		// TODO: support multiple ServerAddresses for replica sets
+		mongodbHost = context.getInitParameter("mongodbHost"); 
+		if (mongodbHost == null || mongodbHost.isEmpty()) {
+			mongodbHost = "localhost";		// only for test environment
 		}
-		if (isPersistent == true) {
-			logger.info("FileServiceProvider is persistent");
-			if (dataF == null) {
-				dataF = new File(context.getRealPath("/" + prefix + DATA_FN));
-			}
-			if (seedF == null) {
-				seedF = new File(context.getRealPath("/" + prefix + SEED_FN));
-			}
+		String _port = context.getInitParameter("mongodbPort");
+		if (_port == null || _port.isEmpty()) {
+			mongodbPort = 27017;
+		} else {
+			mongodbPort = new Integer(_port).shortValue();
 		}
-		else {
-			logger.info("FileServiceProvider is transient");
+		logger.info("AbstractMongodbServiceProvider: mongodbHost=" + mongodbHost + ", mongodbPort=" + mongodbPort);
+	}
+	
+	/**
+	 * Establish a connection to the MongoDB database.
+	 * @throws InternalServerErrorException if the connection could not be established
+	 */
+	protected static void connect()
+			throws InternalServerErrorException 
+	{
+		if (mongoClient != null) {
+			logger.warning("re-connecting to an already open mongoDB connection");
+		} else {
+			try {
+				// TODO: switch to secure mode with authentication
+				// MongoCredential _credential = MongoCredential.createCredential(userName, database, password);
+				// MongoClient _mongoClient = new MongoClient(new ServerAddress(mongodbHost, mongodbPort), Arrays.asList(_credential));
+				mongoClient = new MongoClient(new ServerAddress(mongodbHost, mongodbPort));
+				// default WriteConcern.ACKNOWLEDGED
+				// optionally, choose another WriteConcern eg. _mongoClient.setWriteConcern(WriteConcern.JOURNALED);
+			}
+			catch (MongoException _ex) {
+				logger.info(_ex.getMessage());
+				logger.info(_ex.getStackTrace().toString());		
+				throw new InternalServerErrorException("could not establish a connection to the mongodb: MongoException in AbstractMongodbServiceProvider.constructor");
+			}
 		}
 	}
 	
+	/**
+	 * Set and return the collection with name collectionName. In MongoDB, a collection
+	 * corresponds to a table in a relational database.
+	 * @param collectionName the name of the collection to retrieve.
+	 * @return a collection of Documents
+	 * @throws InternalServerErrorException if there is a problem getting the collection
+	 * @throws ValidationException if the collectionName is not correct
+	 */
+	protected MongoCollection<Document> getCollection(
+			String collectionName) 
+			throws InternalServerErrorException, ValidationException 
+	{
+		if (collectionName == null ||	collectionName.isEmpty()) {
+			throw new ValidationException("collectionName ist not set");
+		}
+		if (mongoClient == null) {
+			throw new InternalServerErrorException("trying to get collection <" + collectionName + "> from non-existing DB; please connect() first.");
+		}
+		try {
+			MongoDatabase _db = mongoClient.getDatabase(DB_NAME);
+			collection = _db.getCollection(collectionName);
+			this.collectionName = collectionName;
+		}
+		catch (Exception _ex) {
+			logger.info(_ex.getMessage());
+			logger.info(_ex.getStackTrace().toString());		
+			throw new InternalServerErrorException("could not get collection <" + collectionName + ">.");			
+		}
+		return collection;
+	}
+	
+	/**
+	 * Return a list of Documents. In MongoDB, a Document corresponds to a row in a relational DB.
+	 * @param position the element to start the return set with
+	 * @param size the amount of documents to return
+	 * @return a list of Documents (ie database objects)
+	 */
+	protected List<Document> list(int position, int size) {
+		// TODO: queries are not yet supported
+		FindIterable<Document> _iterableResult = collection.find().skip(position).limit(size);
+		List<Document> _resultList = new ArrayList<Document>();
+		for (Document _doc : _iterableResult) {
+			_resultList.add(_doc);
+		}
+		logger.info("list(queryObject, " + position + ", " + size + ") -> " + _resultList.size() + " objects.");
+		return _resultList;
+	}
+	
+	/**
+	 * Create a new database object (aka Document). 
+	 * The ID should not be set as it is generated by the MongoDB.
+	 * @param document the object to insert into the database.
+	 */
+	protected void create(Document document) {
+		try {
+			collection.insertOne(document);	
+			logger.info("create(" + document + ") -> OK");
+		}
+		catch (Exception _ex) {
+			logger.info(_ex.getMessage());
+			logger.info(_ex.getStackTrace().toString());		
+			throw new InternalServerErrorException("could not create a dbobject");			
+		}
+	}
+	
+	/**
+	 * Retrieve one single object based on its ID.
+	 * @param id the unique identificator of the object
+	 * @return the object found
+	 * @throws NotFoundException if no object with the given id was found
+	 * @throws InternalServerErrorException if there was a problem getting the object from mongodb
+	 */
+	protected Document readOne(
+			String id) 
+		throws NotFoundException, InternalServerErrorException
+	{
+		Document _result = null;
+		try {
+			// logger.info("readOne(" + id + ")");
+			_result = collection.find(eq("_id", new ObjectId(id))).first();
+		}
+		catch (Exception _ex) {
+			logger.info(_ex.getMessage());
+			logger.info(_ex.getStackTrace().toString());		
+			throw new InternalServerErrorException("could not find dbobject <" + id + ">");			
+		}
+		if (_result == null) {
+			throw new NotFoundException("dbobject with id <" + id + "> was not found in collection " + collectionName + ".");
+		}
+		logger.info("readOne(" + id + ") -> " + _result.toJson());
+		return _result;
+	}
+	
+	/**
+	 * Update the database object with a given unique id.
+	 * @param id the unique identificator of the object to update
+	 * @param document the new data of the database object
+	 * @return the updated object containing the new data
+	 * @throws NotFoundException if no object with the given id was found
+	 * @throws InternalServerErrorException if there was a problem getting the object from mongodb
+	 */
+	protected void update(
+			String id, 
+			Document document) 
+			throws NotFoundException, InternalServerErrorException
+	{
+		UpdateResult _result = null;
+		try {
+			logger.info("update(" + id + ", " + document.toJson() + ")");
+			_result = collection.updateOne(eq("_id", new ObjectId(id)), new Document("$set", document));
+			// TODO: better handling of UpdateResult
+		}
+		catch (Exception _ex) {
+			logger.info(_ex.getMessage());
+			logger.info(_ex.getStackTrace().toString());		
+			throw new InternalServerErrorException("could not update dbobject <" + id + ">");			
+		}
+		if (_result.getModifiedCount() == 0) {
+			throw new NotFoundException("dbobject with id <" + id + "> was not found in collection " + collectionName + ".");
+		}
+		logger.info("update(" + id + ", " + document.toJson() + ") -> OK");
+	}
+	
+	/**
+	 * Delete the database object with a given unique id.
+	 * @param id the unique identificator of the object to delete
+	 * @throws NotFoundException if no object with the given id is found
+	 */
+	protected void deleteOne(
+			String id) 
+		throws NotFoundException
+	{
+		DeleteResult _result = collection.deleteOne(eq("_id", new ObjectId(id)));
+		if (_result.getDeletedCount() == 0) {
+			throw new NotFoundException("dbobject with id <" + id + "> was not found in collection " + collectionName + ".");
+		}
+		logger.info("deleteOne(" + id + ") -> OK");
+	}
+	
+	
+	/**
+	 * Disconnect the MongoDB.
+	 */
+	protected static void disconnect()
+	{
+		if (mongoClient == null) {
+			logger.warning("trying to close a non-existing mongo-connection");
+		} else {
+			mongoClient.close();
+		}
+		logger.info("disconnect() -> OK");
+	}
+	
+	
+	/**
+	 * Retrieve the principal name of the current user.
+	 * @return the principal name
+	 */
 	protected String getPrincipal() 
 	{
 		return "DUMMY_USER";  // TODO: get principal from user session
-	}
-	
-	protected List<T> importJson(
-			File f)  {
-		if (isPersistent == false) {
-			logger.info("importJson(): not importing data (transient)");
-			return new ArrayList<T>();
-		}
-		logger.info("importJson(" + f.getName() + "): importing data");
-		if (!f.exists()) {
-			logger.warning("importJson(" + f.getName()
-					+ "): file does not exist.");
-			return new ArrayList<T>();
-		}
-		if (!f.canRead()) {
-			logger.warning("importJson(" + f.getName()
-					+ "): file is not readable");
-			return new ArrayList<T>();
-		}
-		logger.info("importJson(" + f.getName() + "): can read the file.");
-
-		Reader _reader = null;
-		ArrayList<T> _list = null;
-		try {
-			_reader = new InputStreamReader(new FileInputStream(f));
-			Gson _gson = new GsonBuilder().create();
-			Type _collectionType = new TypeToken<ArrayList<T>>() {}.getType();
-			_list = _gson.fromJson(_reader, _collectionType);
-			logger.info("importJson(" + f.getName() + "): json data converted to type " + _collectionType.toString());
-		} catch (FileNotFoundException e1) {
-			logger.severe("importJson(" + f.getName()
-					+ "): file does not exist (2).");
-			e1.printStackTrace();
-		} finally {
-			try {
-				if (_reader != null) {
-					_reader.close();
-				}
-			} catch (IOException e) {
-				logger.severe("importJson(" + f.getName()
-						+ "): IOException when closing the reader.");
-				e.printStackTrace();
-			}
-		}
-		logger.info("importJson(" + f.getName() + "): " + _list.size()
-				+ " objects imported.");
-		return _list;
-	}
-
-	
-	protected List<T> importJson() throws IOException {
-		if (isPersistent == false) {
-			logger.info("importJson(): not importing data (transient)");
-			return new ArrayList<T>();
-		}
-		List<T> _objects = null;
-		
-		// read the data file
-		// either read persistent data from DATA_FN
-		// or seed data from SEED_FN if no persistent data exists
-		if (dataF.exists()) {
-			logger.info("persistent data in file " + dataF.getName()
-					+ " exists.");
-			_objects = importJson(dataF);
-		} else { // seeding the data
-			logger.info("persistent data in file " + dataF.getName()
-					+ " is missing -> trying to seed from " + seedF.getName());
-			// importing the seed data
-			_objects = importJson(seedF);
-			// create the persistent data if it did not exist
-			if (isPersistent == true) {
-				try {
-					dataF.createNewFile();					
-					exportJson(_objects);
-				} 
-				catch (IOException _e) {
-					logger.severe("importJson(): IO exception when creating file "
-							+ dataF.getName());
-					_e.printStackTrace();
-				}
-			}
-		}
-
-		logger.info("importJson(): imported " + _objects.size()
-				+ " objects");
-		return _objects;
-	}
-
-	protected void exportJson(Collection<T> objects) {
-		if (isPersistent == false) {
-			logger.info("exportJson(): not exporting data (transient)");
-			return;
-		}
-		logger.info("exportJson(" + dataF.getName() + "): exporting objects in json format");
-
-		Writer _writer = null;
-		try {
-			_writer = new OutputStreamWriter(new FileOutputStream(dataF));
-			Gson _gson = new GsonBuilder().setPrettyPrinting().create();
-			_gson.toJson(objects, _writer);
-		} catch (FileNotFoundException e) {
-			logger.severe("exportJson(" + dataF.getName() + "): file not found.");
-			e.printStackTrace();
-		} finally {
-			if (_writer != null) {
-				try {
-					_writer.close();
-				} catch (IOException e) {
-					logger.severe("exportJson(" + dataF.getName()
-							+ "): IOException when closing the reader.");
-					e.printStackTrace();
-				}
-			}
-		}
 	}
 }
